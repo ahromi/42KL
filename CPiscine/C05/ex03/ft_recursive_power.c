fbd3b610d0e9ad2708a4714a7b3cb857513318f4
ahromi <ahromi@gmail.com>
2024-09-08 21:59:13 +0800

    modified ex00 & ex02 | add ft_recursive_power.c file as Exercise 03

--- CPiscine/C05/ex03/ft_recursive_power.c
+++ CPiscine/C05/ex03/ft_recursive_power.c
@@ -1,40 +1,40 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_iterative_power.c                               :+:      :+:    :+:   */
+/*   ft_recursive_power.c                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: iahromi <iahromi@student.42kl.edu.my>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/09/08 03:41:53 by iahromi           #+#    #+#             */
-/*   Updated: 2024/09/08 21:55:06 by iahromi          ###   ########.fr       */
+/*   Created: 2024/09/08 21:36:31 by iahromi           #+#    #+#             */
+/*   Updated: 2024/09/08 21:55:28 by iahromi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+/*
+#include <stdio.h>
 
-int	ft_iterative_power(int nb, int power)
-{
-	int	i;
-	int	result;
+int	ft_recursive_power(int nb, int power);
 
-	i = 0;
-	result = nb;
-	if (power < 0)
-		return (0);
-	if (power == 0 || (nb == 0 && power == 0))
-		return (1);
-	while (++i < power)
-		result *= nb;
-	return (result);
-}
-/*#include <stdio.h>
 int	main(void)
 {
 	int	nb;
 	int	power;
 	int	result;
 
-	nb = 5;
+	nb = 2;
 	power = 1;
-	result = ft_iterative_power(nb, power);
+	result = ft_recursive_power(nb, power);
 	printf("%d ^ %d is: %d\n", nb, power, result);
 	return (0);
-}*/
+}
+*/
+int	ft_recursive_power(int nb, int power)
+{
+	if (power < 0)
+		return (0);
+	else if (power == 0 || (nb == 0 && power == 0))
+		return (1);
+	else if (power == 1)
+		return (nb);
+	else
+		return (ft_recursive_power(nb, power - 1) * nb);
+}
