e2c4436ff299e6691075aea1c0cdaba3ca1cedf7
Irfan Ahromi <ahromi@gmail.com>
2024-10-10 21:27:54 +0800

    add ft_ultimate_ft.c as Exercise 01 ex01

---
+++ CPiscine/C01/ex01/ft_ultimate_ft.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ultimate_ft.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: iahromi <iahromi@student.42kl.edu.my>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/05 18:22:15 by iahromi           #+#    #+#             */
+/*   Updated: 2024/10/10 21:26:54 by iahromi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_ultimate_ft(int *********nbr)
+{
+	*********nbr = 42;
+}
+/*
+#include <stdio.h>
+
+int	main(void)
+{
+	int	nbr9;
+	int	*nbr8;
+	int	**nbr7;
+	int	***nbr6;
+	int	****nbr5;
+	int	*****nbr4;
+	int	******nbr3;
+	int	*******nbr2;
+	int	********nbr1;
+	int	*********nbr;
+
+	nbr9 = 200;
+	nbr8 = &nbr9;
+	nbr7 = &nbr8;
+	nbr6 = &nbr7;
+	nbr5 = &nbr6;
+	nbr4 = &nbr5;
+	nbr3 = &nbr4;
+	nbr2 = &nbr3;
+	nbr1 = &nbr2;
+	nbr = &nbr1;
+	ft_ultimate_ft(nbr);
+	printf("The value of nbr is: %d\n", nbr9);
+	return (0);
+}*/
