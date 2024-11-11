99055085d2c5f1ba5fe06d059731553b00a6801f
Irfan Ahromi <ahromi@gmail.com>
2024-11-11 10:59:22 +0800

    add libft.h initiate

---
+++ Core/Libft/libft.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: iahromi <iahromi@student.42kl.edu.my>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/11 10:54:13 by iahromi           #+#    #+#             */
+/*   Updated: 2024/11/11 10:58:41 by iahromi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+#define LIBFT_H
+#include <stdlib.h>
+#include <unistd.h>
+
+int	ft_isalpha(int x);
+int	ft_isdigit(int x);
+int	ft_isalnum(int x);
+int	ft_isascii(int x);
+int	ft_isprint(int x);
+
+#endif
