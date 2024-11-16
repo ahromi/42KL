9090cf2e705ea00ce93541b20b9d3c2b124180c5
Irfan Ahromi <ahromi@gmail.com>
2024-11-16 18:15:22 +0800

    add bonus file: ft_lstdelone.c

---
+++ Core/Libft/ft_lstdelone.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstdelone.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: iahromi <iahromi@student.42kl.edu.my>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/16 18:10:31 by iahromi           #+#    #+#             */
+/*   Updated: 2024/11/16 18:13:22 by iahromi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstdelone(t_list *lst, void (*del)(void *))
+{
+	if (!lst || !new)
+		return ;
+	if (lst->content)
+	{
+		(*del)(lst->content);
+		free(lst);
+	}
+}
