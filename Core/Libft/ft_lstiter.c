71f0a923ccf8a19f2ab3054b72459c5b1f7be02b
Irfan Ahromi <ahromi@gmail.com>
2024-11-16 18:38:29 +0800

    add bonus file: ft_lstiter.c

---
+++ Core/Libft/ft_lstiter.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstiter.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: iahromi <iahromi@student.42kl.edu.my>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/16 18:26:43 by iahromi           #+#    #+#             */
+/*   Updated: 2024/11/16 18:37:08 by iahromi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstiter(t_list *lst, void (*f)(void *))
+{
+	t_list	*tmp;
+
+	tmp = lst;
+	if (!lst || !f)
+		return ;
+	while (tmp)
+	{
+		(*f)(lst->content);
+		tmp = tmp->next;
+	}
+}
