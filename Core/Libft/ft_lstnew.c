7ddf76733a6913552e3f66d3ef73727fee45ea43
Irfan Ahromi <ahromi@gmail.com>
2024-11-16 17:31:39 +0800

    add bonus file: ft_lstnew.c

---
+++ Core/Libft/ft_lstnew.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: iahromi <iahromi@student.42kl.edu.my>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/16 17:25:23 by iahromi           #+#    #+#             */
+/*   Updated: 2024/11/16 17:28:22 by iahromi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list *element;
+
+	element = malloc(sizeof(t_list));
+	if (!element)
+		return (NULL);
+	element->content = content;
+	element->next = NULL;
+	return (element);
+}
