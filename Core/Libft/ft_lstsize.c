4c84a816e856d9b33381bda173917a75ba2b56fb
Irfan Ahromi <ahromi@gmail.com>
2024-11-16 17:49:50 +0800

    add bonus file: ft_lstsize.c

---
+++ Core/Libft/ft_lstsize.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstsize.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: iahromi <iahromi@student.42kl.edu.my>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/16 17:44:08 by iahromi           #+#    #+#             */
+/*   Updated: 2024/11/16 17:48:42 by iahromi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_lstsize(t_list *lst)
+{
+	int	size;
+
+	if (!lst)
+		return (0);
+	size = 1;
+	if (lst->next)
+		size += ft_lstsize(lst->next);
+	return (size);
+}
